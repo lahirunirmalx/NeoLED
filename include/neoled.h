5e1241578bb0d327c5e36f05f1cca68d801b7c3d
lahiru <lahirunirmalx@gmail.com>
2024-11-13 16:25:42 +0530

    init commit for neo LED

---
+++ include/neoled.h
@@ -0,0 +1,108 @@
+/** MIT licence
+
+ Copyright (C) 2019 by Vu Nam https://github.com/vunam https://studiokoda.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions: The above copyright notice and this
+ permission notice shall be included in all copies or substantial portions of
+ the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+
+*/
+#ifndef NEOLED_H
+#define NEOLED_H
+
+#include <cstdint>
+
+namespace NeoLED {
+
+#define LED_NUMBER 1
+#define PIXEL_SIZE 12 // each color takes 4 bytes
+#define SAMPLE_RATE (93750)
+#define ZERO_BUFFER 48
+#define I2S_NUM (0)
+#define I2S_DO_IO (21)
+
+
+#define COLOR_RED        (Pixel){0, 255, 0}
+#define COLOR_ORANGE     (Pixel){64, 255, 0}
+#define COLOR_YELLOW     (Pixel){128, 255, 0}
+#define COLOR_LIME       (Pixel){255, 255, 0}
+#define COLOR_GREEN      (Pixel){255, 0, 0}
+#define COLOR_TURQUOISE  (Pixel){255, 0, 128}
+#define COLOR_CYAN       (Pixel){255, 0, 255}
+#define COLOR_AQUA       (Pixel){128, 0, 255}
+#define COLOR_BLUE       (Pixel){0, 0, 255}
+#define COLOR_PURPLE     (Pixel){0, 128, 255}
+#define COLOR_MAGENTA    (Pixel){0, 255, 255}
+#define COLOR_ROSE       (Pixel){0, 255, 128}
+#define COLOR_WHITE      (Pixel){255, 255, 255}
+#define COLOR_OFF        (Pixel){0, 0, 0}
+
+
+#define HUE_RED        0     // Red
+#define HUE_ORANGE     32    // Orange
+#define HUE_YELLOW     64    // Yellow
+#define HUE_LIME       80    // Lime Green
+#define HUE_GREEN      96    // Green
+#define HUE_TURQUOISE  112   // Turquoise
+#define HUE_CYAN       128   // Cyan
+#define HUE_AQUA       144   // Aqua Blue
+#define HUE_BLUE       160   // Blue
+#define HUE_PURPLE     176   // Purple
+#define HUE_MAGENTA    192   // Magenta (Pink)
+#define HUE_ROSE       224   // Rose Pink
+#define HUE_WHITE      0     // Use full brightness for white (no hue shift)
+#define HUE_OFF        0     // Turn off LED (set RGB values to zero)
+
+typedef struct {
+    uint8_t green;
+    uint8_t red;
+    uint8_t blue;
+} Pixel;
+
+void init();
+void update(Pixel* pixels); 
+
+inline Pixel makePixel(uint8_t r, uint8_t g, uint8_t b) {
+    Pixel pixel;
+    pixel.red = r;
+    pixel.green = g;
+    pixel.blue = b;
+    return pixel;
+}
+inline Pixel colorWheel(uint8_t hue) {
+     Pixel pixel;
+
+    if (hue < 85) {
+        pixel.red = hue * 3;
+        pixel.green = 255 - hue * 3;
+        pixel.blue = 0;
+    } else if (hue < 170) {
+        hue -= 85;
+        pixel.red = 255 - hue * 3;
+        pixel.green = 0;
+        pixel.blue = hue * 3;
+    } else {
+        hue -= 170;
+        pixel.red = 0;
+        pixel.green = hue * 3;
+        pixel.blue = 255 - hue * 3;
+    }
+
+    return pixel;
+}
+} // namespace NeoLED
+
+#endif // NEOLED_H
---
+++ neoled.cpp
@@ -0,0 +1,98 @@
+/** MIT licence
+
+ Copyright (C) 2019 by Vu Nam https://github.com/vunam https://studiokoda.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights to
+ use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ of the Software, and to permit persons to whom the Software is furnished to do
+ so, subject to the following conditions: The above copyright notice and this
+ permission notice shall be included in all copies or substantial portions of
+ the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+ EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+ OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ DEALINGS IN THE SOFTWARE.
+
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/i2s.h"
+#include "driver/gpio.h"   
+#include "neoled.h"
+
+namespace NeoLED {
+
+static uint8_t out_buffer[LED_NUMBER * PIXEL_SIZE] = {0};
+static uint8_t off_buffer[ZERO_BUFFER] = {0};
+static uint16_t size_buffer;
+
+static const uint16_t bitpatterns[4] = {0x88, 0x8e, 0xe8, 0xee};
+
+i2s_config_t i2s_config = {
+    .mode = static_cast<i2s_mode_t>(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = static_cast<i2s_comm_format_t> ( I2S_COMM_FORMAT_STAND_I2S | I2S_COMM_FORMAT_STAND_MSB),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 4,
+    .dma_buf_len = LED_NUMBER * PIXEL_SIZE,
+    .use_apll = false,
+    .mclk_multiple = I2S_MCLK_MULTIPLE_DEFAULT
+};
+
+i2s_pin_config_t pin_config = {
+    .mck_io_num = static_cast<gpio_num_t>(1),
+    .bck_io_num = static_cast<gpio_num_t>(-1),
+    .ws_io_num = static_cast<gpio_num_t>(-1),
+    .data_out_num = static_cast<gpio_num_t>(I2S_DO_IO),
+    .data_in_num = static_cast<gpio_num_t>(-1)
+    
+};
+
+void init() {
+    size_buffer = LED_NUMBER * PIXEL_SIZE;
+    i2s_driver_install(static_cast<i2s_port_t>(I2S_NUM), &i2s_config, 0, nullptr);
+    i2s_set_pin(static_cast<i2s_port_t>(I2S_NUM), &pin_config);
+}
+
+void update(Pixel* pixels) {
+    size_t bytes_written = 0;
+
+    for (uint16_t i = 0; i < LED_NUMBER; i++) {
+        int loc = i * PIXEL_SIZE;
+
+        out_buffer[loc] = bitpatterns[pixels[i].green >> 6 & 0x03];
+        out_buffer[loc + 1] = bitpatterns[pixels[i].green >> 4 & 0x03];
+        out_buffer[loc + 2] = bitpatterns[pixels[i].green >> 2 & 0x03];
+        out_buffer[loc + 3] = bitpatterns[pixels[i].green & 0x03];
+
+        out_buffer[loc + 4] = bitpatterns[pixels[i].red >> 6 & 0x03];
+        out_buffer[loc + 5] = bitpatterns[pixels[i].red >> 4 & 0x03];
+        out_buffer[loc + 6] = bitpatterns[pixels[i].red >> 2 & 0x03];
+        out_buffer[loc + 7] = bitpatterns[pixels[i].red & 0x03];
+
+        out_buffer[loc + 8] = bitpatterns[pixels[i].blue >> 6 & 0x03];
+        out_buffer[loc + 9] = bitpatterns[pixels[i].blue >> 4 & 0x03];
+        out_buffer[loc + 10] = bitpatterns[pixels[i].blue >> 2 & 0x03];
+        out_buffer[loc + 11] = bitpatterns[pixels[i].blue & 0x03];
+    }
+
+    i2s_write(static_cast<i2s_port_t>(I2S_NUM), out_buffer, size_buffer, &bytes_written, portMAX_DELAY);
+    i2s_write(static_cast<i2s_port_t>(I2S_NUM), off_buffer, ZERO_BUFFER, &bytes_written, portMAX_DELAY);
+    vTaskDelay(pdMS_TO_TICKS(10));
+    i2s_zero_dma_buffer(static_cast<i2s_port_t>(I2S_NUM));
+}
+
+} // namespace NeoLED
